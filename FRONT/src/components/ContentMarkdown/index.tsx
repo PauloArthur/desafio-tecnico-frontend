97f9d4b1e6b9f88ad2773d92a43c830f7b955e1f
Paulo Arthur <pauloarthurcostamendes@gmail.com>
2023-12-10 23:41:25 +0000

    applying Tomorrow Light and Dark themes to code inside markdown

--- FRONT/src/components/ContentMarkdown/index.tsx
+++ FRONT/src/components/ContentMarkdown/index.tsx
@@ -3,11 +3,18 @@ import { Container } from './styles';
 import Markdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import rehypeHighlight from 'rehype-highlight';
+import { useTheme } from '../../contexts/ThemeProvider';
 
 export const ContentMarkdown = ({ value }: { value: string }) => {
+  const { isDarkMode } = useTheme();
   return (
-    <Container className="Markdown-content markdown-body">
-      <Markdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeHighlight]}>
+    <Container
+      className={`${isDarkMode ? 'dark-theme' : 'light-theme'} markdown-body`}
+    >
+      <Markdown
+        remarkPlugins={[remarkGfm]}
+        rehypePlugins={[[rehypeHighlight, { prefix: '' }]]}
+      >
         {value}
       </Markdown>
     </Container>
--- FRONT/src/components/ContentMarkdown/styles.ts
+++ FRONT/src/components/ContentMarkdown/styles.ts
@@ -1,4 +1,178 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+const lightThemeMarkdown = css`
+  /* Tomorrow Theme */
+  /* Original theme - https://github.com/chriskempson/tomorrow-theme */
+  /* https://jmblog.github.io/color-themes-for-google-code-highlightjs */
+  .tomorrow-comment,
+  pre .comment,
+  pre .title {
+    color: #8e908c;
+  }
+
+  .tomorrow-red,
+  pre .variable,
+  pre .attribute,
+  pre .tag,
+  pre .regexp,
+  pre .ruby .constant,
+  pre .xml .tag .title,
+  pre .xml .pi,
+  pre .xml .doctype,
+  pre .html .doctype,
+  pre .css .id,
+  pre .css .class,
+  pre .css .pseudo {
+    color: #c82829;
+  }
+
+  .tomorrow-orange,
+  pre .number,
+  pre .preprocessor,
+  pre .built_in,
+  pre .literal,
+  pre .params,
+  pre .constant {
+    color: #f5871f;
+  }
+
+  .tomorrow-yellow,
+  pre .class,
+  pre .ruby .class .title,
+  pre .css .rules .attribute {
+    color: #eab700;
+  }
+
+  .tomorrow-green,
+  pre .string,
+  pre .value,
+  pre .inheritance,
+  pre .header,
+  pre .ruby .symbol,
+  pre .xml .cdata {
+    color: #718c00;
+  }
+
+  .tomorrow-aqua,
+  pre .css .hexcolor {
+    color: #3e999f;
+  }
+
+  .tomorrow-blue,
+  pre .function,
+  pre .python .decorator,
+  pre .python .title,
+  pre .ruby .function .title,
+  pre .ruby .title .keyword,
+  pre .perl .sub,
+  pre .javascript .title,
+  pre .coffeescript .title {
+    color: #4271ae;
+  }
+
+  .tomorrow-purple,
+  pre .keyword,
+  pre .javascript .function {
+    color: #8959a8;
+  }
+
+  pre code {
+    display: block;
+    background: white;
+    color: #4d4d4c;
+    font-family: Menlo, Monaco, Consolas, monospace;
+    line-height: 1.5;
+    border: 1px solid #ccc;
+    padding: 10px;
+  }
+`;
+
+const darkThemeMarkdown = css`
+  /* Tomorrow Night Theme */
+  /* Original theme - https://github.com/chriskempson/tomorrow-theme */
+  /* https://jmblog.github.io/color-themes-for-google-code-highlightjs */
+  .tomorrow-comment,
+  pre .comment,
+  pre .title {
+    color: #969896;
+  }
+
+  .tomorrow-red,
+  pre .variable,
+  pre .attribute,
+  pre .tag,
+  pre .regexp,
+  pre .ruby .constant,
+  pre .xml .tag .title,
+  pre .xml .pi,
+  pre .xml .doctype,
+  pre .html .doctype,
+  pre .css .id,
+  pre .css .class,
+  pre .css .pseudo {
+    color: #cc6666;
+  }
+
+  .tomorrow-orange,
+  pre .number,
+  pre .preprocessor,
+  pre .built_in,
+  pre .literal,
+  pre .params,
+  pre .constant {
+    color: #de935f;
+  }
+
+  .tomorrow-yellow,
+  pre .class,
+  pre .ruby .class .title,
+  pre .css .rules .attribute {
+    color: #f0c674;
+  }
+
+  .tomorrow-green,
+  pre .string,
+  pre .value,
+  pre .inheritance,
+  pre .header,
+  pre .ruby .symbol,
+  pre .xml .cdata {
+    color: #b5bd68;
+  }
+
+  .tomorrow-aqua,
+  pre .css .hexcolor {
+    color: #8abeb7;
+  }
+
+  .tomorrow-blue,
+  pre .function,
+  pre .python .decorator,
+  pre .python .title,
+  pre .ruby .function .title,
+  pre .ruby .title .keyword,
+  pre .perl .sub,
+  pre .javascript .title,
+  pre .coffeescript .title {
+    color: #81a2be;
+  }
+
+  .tomorrow-purple,
+  pre .keyword,
+  pre .javascript .function {
+    color: #b294bb;
+  }
+
+  pre code {
+    display: block;
+    background: #1d1f21;
+    color: #c5c8c6;
+    font-family: Menlo, Monaco, Consolas, monospace;
+    line-height: 1.5;
+    border: 1px solid #ccc;
+    padding: 10px;
+  }
+`;
 
 export const Container = styled.div`
   height: 100%;
@@ -8,4 +182,10 @@ export const Container = styled.div`
   overflow: scroll;
   color: ${({ theme }) => theme.colors.text}E6;
   background-color: ${({ theme }) => theme.colors.text}1A;
+  &.dark-theme {
+    ${darkThemeMarkdown}
+  }
+  &.light-theme {
+    ${lightThemeMarkdown}
+  }
 `;
